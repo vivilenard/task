07b9fc597af9e81fdccf2d8f28f99ca27234f44a
Vivi <lenardvivien@yahoo.de>
2024-12-22 15:11:03 +0100

    made my own string generator

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"github.com/labstack/echo/v4"
 	"github.com/a-h/templ"
-	"io"
+	"math/rand"
 )
 
 
@@ -18,20 +17,14 @@ func main() {
 	e.Logger.Fatal(e.Start(":3000"))
 }
 
-func randomStringHandler(c echo.Context) error {
-	url := "https://osob.de/r"
-
-	resp, err := http.Get(url)
-	if err != nil {
-		fmt.Printf("Error making GET request: %v", err)
+func randomStringHandler(c echo.Context) error{
+	charSet := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	randomString := ""
+	stringLength := 100
+	
+	for i := 0; i < stringLength; i++ {
+		randomInt := rand.Intn(len(charSet) - 1)
+		randomString += string(charSet[randomInt])
 	}
-	defer resp.Body.Close()
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("Error: Received status code %d", resp.StatusCode)
-	}
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Printf("Error reading response body: %v", err)
-	}
-	return c.String(http.StatusOK, string(body))
+	return c.String(http.StatusOK, randomString)
 }
